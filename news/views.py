1da811cfab7bb12205dbd4895c3c06f886fc7f34
artvdovin <130549394+artvdovin@users.noreply.github.com>
2025-02-05 16:08:24 +0800

    Add files via upload

--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView
+from .models import Post
 # Create your views here.
+class News(ListView):
+    model = Post
+    ordering = '-date_add'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+class NewsDetail (DetailView):
+    model=Post
+    template_name = 'new.html'
+    context_object_name = 'new'
+
